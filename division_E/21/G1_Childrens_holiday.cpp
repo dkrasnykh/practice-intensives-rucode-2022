38eb6201635743d6e9be59521c71457f14cd0fa4
Darya Krasnykh <krasnykh.daria@gmail.com>
2023-01-18 13:40:33 +0300

    G1

--- division_E/21/G1_Childrens_holiday.cpp
+++ division_E/21/G1_Childrens_holiday.cpp
@@ -3,44 +3,47 @@
 
 using namespace std;
 
-bool check(int m, int k, int t[], int z[], int y[], int n){
-    int cnt=0;
-    for(int i = 0; i<n; i++){
-        int part = (((m%(t[i]*z[i]+y[i]))/t[i])<  z[i])?(m%(t[i]*z[i]+y[i])/t[i]):z[i];
-        cnt+=z[i]*(m/(t[i]*z[i]+y[i]))+part;
+bool check(int m, int k, int t[], int z[], int y[], int n) {
+    //int cnt = 0;
+    long long cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int part = (((m % (t[i] * z[i] + y[i])) / t[i]) < z[i]) ? (m % (t[i] * z[i] + y[i]) / t[i]) : z[i];
+        cnt += static_cast<long long>(z[i] * (m / (t[i] * z[i] + y[i])) + part);
     }
-    return cnt>=k;
+    return cnt >= static_cast<long long>(k);
 }
 
 int binsearch(int l, int r, int k, int t[], int z[], int y[], int n) {
     while (l < r) {
-        int m = (l + r) / 2;
+        //int m = (l + r) / 2;
+        int m = l / 2 + r / 2;
         if (check(m, k, t, z, y, n)) {
             r = m;
-        } else {
+        }
+        else {
             l = m + 1;
         }
     }
     return l;
 }
 
-int main(){
+int main() {
     int m, n;
-    cin>>m>>n;
+    cin >> m >> n;
     int t[n];
     int z[n];
     int y[n];
-    for(int i = 0; i<n; i++){
-        cin>>t[i]>>z[i]>>y[i];
+    for (int i = 0; i < n; i++) {
+        cin >> t[i] >> z[i] >> y[i];
     }
     int r = std::numeric_limits<int>::max();
-    int ans = binsearch(0, r, m, t, z,y, n);
-    cout<<ans<<'\n';
-    int cnt=m;
-    for(int i = 0; i<n; i++){
-        int part = ((ans%(t[i]*z[i]+y[i])/t[i])< z[i])?(ans%(t[i]*z[i]+y[i])/t[i]):z[i];
-        int res = (z[i]*(ans/(t[i]*z[i]+y[i]))+part);
-        cout<<((cnt<=res)?((cnt<0)?0:cnt):res)<<' ';
-        cnt-=res;
+    int ans = binsearch(0, r, m, t, z, y, n);
+    cout << ans << '\n';
+    int cnt = m;
+    for (int i = 0; i < n; i++) {
+        int part = ((ans % (t[i] * z[i] + y[i]) / t[i]) < z[i]) ? (ans % (t[i] * z[i] + y[i]) / t[i]) : z[i];
+        int res = (z[i] * (ans / (t[i] * z[i] + y[i])) + part);
+        cout << ((cnt <= res) ? ((cnt < 0) ? 0 : cnt) : res) << ' ';
+        cnt -= res;
     }
 }
